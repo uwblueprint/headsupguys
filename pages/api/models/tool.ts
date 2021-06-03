dd2faa23b508fb8c5b90b4d76a557896e515cabf
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-06-02 22:37:10 -0400

    create tool model

---
+++ pages/api/models/tool.ts
@@ -0,0 +1,51 @@
+import { model, Schema, Types } from "mongoose";
+
+enum StatusType {
+    DRAFT = "draft",
+    PUBLISHED = "published",
+}
+
+interface ToolInterface {
+    _id: Types.ObjectId;
+    title: string;
+    video: string;
+    description: string;
+    moduleID: Schema.Types.ObjectId;
+    resources: [{ title: string; description: string; url: string }];
+    selfCheckGroupID: Schema.Types.ObjectId;
+    relatedToolsIDs: [Schema.Types.ObjectId];
+    status: StatusType;
+    editing: boolean;
+}
+
+const ToolSchema = new Schema<ToolInterface>(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        video: String,
+        description: String,
+        moduleID: { type: Schema.Types.ObjectId, ref: "Module" },
+        resources: [{ title: String, description: String, url: String }],
+        selfCheckGroupID: {
+            type: Schema.Types.ObjectId,
+            ref: "SelfCheckGroup",
+        },
+        relatedToolsIDs: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Tool",
+            },
+        ],
+        status: StatusType,
+        editing: Boolean,
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const Tool = model("Tool", ToolSchema);
+
+export { Tool };

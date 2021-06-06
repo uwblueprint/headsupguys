c319ad171ce9bd07f6a407f3b2d3fa3291f2b31c
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-06-06 04:40:21 -0400

    Update module.ts

--- pages/api/models/module.ts
+++ pages/api/models/module.ts
@@ -12,34 +12,35 @@ export interface ModuleInterface {
     _id: Schema.Types.ObjectId;
     title: string;
     tool: ToolInterface["_id"];
-    slides: SlideInterface["_id"];
+    slides: [SlideInterface["_id"]];
     status: StatusType;
     editing: boolean;
 }
 
-const ModuleSchema = new Schema<ModuleInterface>({
-    title: {
-        type: String,
-        required: true,
+const ModuleSchema = new Schema<ModuleInterface>(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        tool: {
+            type: Schema.Types.ObjectId,
+        },
+        slides: {
+            type: [{ type: Schema.Types.ObjectId, ref: "Slide" }]
+        },
+        status: {
+            type: StatusType,
+            default: "draft",
+        },
+        editing: {
+            type: Boolean
+        },
     },
-    tool: {
-        type: Schema.Types.ObjectId,
-    },
-    slides: {
-        type: [{ type: Schema.Types.ObjectId, ref: "Slide" }],
-        default: [],
-    },
-    status: {
-        type: StatusType,
+    {
         timestamps: true,
-        default: "draft",
-        required: true,
     },
-    editing: {
-        type: Boolean,
-        required: true,
-    },
-});
+);
 
 const Module = model("Module", ModuleSchema);
 

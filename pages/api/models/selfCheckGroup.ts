8d5a9f795fa078c57cdcf54fcb39c21c9fb44141
Christine Ng <69013315+christineNg1028@users.noreply.github.com>
2021-06-09 19:44:13 -0400

    [DB Schema] Create Self Check Model (#9)
    * self check question + group models
    
    * Update selfCheckQuestion.ts
    
    * changes to SelfCheckGroup and SelfCheckQuestion
    
    * Update selfCheckGroup.ts
    
    * Update pages/api/models/selfCheckQuestion.ts
    
    Co-authored-by: Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
    
    * Update selfCheckQuestion.ts
    
    Co-authored-by: Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>

---
+++ pages/api/models/selfCheckGroup.ts
@@ -0,0 +1,22 @@
+import { model, Schema, Types } from "mongoose";
+
+interface SelfCheckGroupInterface {
+    _id: Types.ObjectId;
+    questionIDs: [Types.ObjectId];
+}
+
+const SelfCheckGroupSchema = new Schema<SelfCheckGroupInterface>(
+    {
+        questionIDs: {
+            type: [{ type: Schema.Types.ObjectId, ref: "SelfCheckQuestion" }],
+            default: [],
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const SelfCheckGroup = model("SelfCheckGroup", SelfCheckGroupSchema);
+
+export { SelfCheckGroup };
---
+++ pages/api/models/selfCheckQuestion.ts
@@ -0,0 +1,44 @@
+import { model, Schema, Types } from "mongoose";
+
+enum QuestionType {
+    MULTIPLE_CHOICE = "multiple_choice",
+    MULTI_SELECT = "multi_select",
+}
+
+interface SelfCheckQuestionInterface {
+    _id: Types.ObjectId;
+    type: QuestionType;
+    question: string;
+    options: [Schema.Types.Mixed];
+    questionNumber: number;
+}
+
+const SelfCheckQuestionSchema = new Schema<SelfCheckQuestionInterface>(
+    {
+        type: {
+            type: String,
+            enum: Object.values(QuestionType),
+            required: true,
+        },
+        question: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        options: {
+            type: [Schema.Types.Mixed],
+            default: [],
+        },
+        questionNumber: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const SelfCheckQuestion = model("SelfCheckQuestion", SelfCheckQuestionSchema);
+
+export { SelfCheckQuestion };

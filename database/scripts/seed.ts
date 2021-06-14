874cca9e04a152ce17803812d30552e5df2b9a03
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-06-13 23:34:43 -0400

    use awaits for collection methods

--- database/scripts/seed.ts
+++ database/scripts/seed.ts
@@ -2,25 +2,26 @@
 require("dotenv").config({ path: ".env.local" });
 const MongoClient = require("mongodb").MongoClient;
 
-const client = new MongoClient(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-});
-
 (async function () {
+    const client = new MongoClient(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
     try {
         await client.connect();
-        console.log("Successfully connected to server");
+        console.log("Successfully connected to database");
 
         const componentsCollection = client
             .db(process.env.MONGODB_DB)
             .collection("components");
-        componentsCollection.drop(); // destroy previous data
-        componentsCollection.insertMany(mockComponents(50));
+        await componentsCollection.drop(); // destroy previous data
+        await componentsCollection.insertMany(mockComponents(50));
+
+        // TODO add data to other collections
 
         console.log("Successfully completed seeding");
         client.close();
-
-        // TODO add data to other collections
     } catch (err) {
         console.log(err.stack);
     }

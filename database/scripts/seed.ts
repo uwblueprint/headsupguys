51b4d181f11a192049e7380968c503a6a9daa126
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-06-16 16:35:54 -0400

    mock question groups

--- database/scripts/seed.ts
+++ database/scripts/seed.ts
@@ -4,7 +4,9 @@ const MongoClient = require("mongodb").MongoClient;
 const faker = require("faker");
 
 // seed config
-const FIRST_RUN = true;
+const FIRST_RUN = false;
+const QUESTION_COUNT = 18;
+const QUESTION_GROUP_SIZE = 5;
 
 (async function () {
     const client = new MongoClient(process.env.MONGODB_URI, {
@@ -16,24 +18,26 @@ const FIRST_RUN = true;
         await client.connect();
         console.log("Successfully connected to database");
 
-        const componentsCollection = client
-            .db(process.env.MONGODB_DB)
-            .collection("components");
-
-        const questionsCollection = client
-            .db(process.env.MONGODB_DB)
-            .collection("SelfCheckQuestion");
+        const db = client.db(process.env.MONGODB_DB);
+        const componentsCollection = db.collection("components");
+        const questionsCollection = db.collection("SelfCheckQuestion");
+        const groupsCollection = db.collection("SelfCheckGroup");
 
         if (!FIRST_RUN) {
             // destroy previous data
             await Promise.all([
-                componentsCollection.drop(),
                 questionsCollection.drop(),
+                groupsCollection.drop(),
+                componentsCollection.drop(),
             ]);
         }
 
+        await questionsCollection.insertMany(mockQuestions(QUESTION_COUNT));
+        await groupsCollection.insertMany(
+            mockGroups(Math.floor(QUESTION_COUNT / QUESTION_GROUP_SIZE)),
+        );
+
         await componentsCollection.insertMany(mockComponents(50));
-        await questionsCollection.insertMany(mockQuestions(18));
 
         // TODO add data to other collections
 
@@ -43,19 +47,6 @@ const FIRST_RUN = true;
         console.log(err.stack);
     }
 })(); // immediately-invoked function expression
-
-function mockComponents(count) {
-    const componentTypes = ["text", "video", "audio"];
-    const components = [];
-
-    for (let i = 0; i < count; i++) {
-        components.push({
-            type: componentTypes[i % componentTypes.length],
-            properties: {},
-        });
-    }
-    return components;
-}
 
 function mockQuestions(count) {
     const questionTypes = ["multiple_choice", "multi_select"];
@@ -67,11 +58,41 @@ function mockQuestions(count) {
 
     for (let i = 0; i < count; i++) {
         questions.push({
+            _id: i,
             type: questionTypes[i % questionTypes.length],
             question: faker.lorem.sentence(),
             options: mockOptions[i % mockOptions.length],
-            questionNumber: (i % 5) + 1,
+            questionNumber: (i % QUESTION_GROUP_SIZE) + 1,
         });
     }
     return questions;
+}
+
+function mockGroups(count) {
+    const groups = [];
+
+    for (let i = 0; i < count; i++) {
+        const questionIDs = [];
+
+        for (let j = 0; j < QUESTION_GROUP_SIZE; j++) {
+            questionIDs.push(i * QUESTION_GROUP_SIZE + j);
+        }
+
+        groups.push({ questionIDs });
+    }
+    return groups;
+}
+
+function mockComponents(count) {
+    const componentTypes = ["text", "video", "audio"];
+    const components = [];
+
+    for (let i = 0; i < count; i++) {
+        components.push({
+            _id: i,
+            type: componentTypes[i % componentTypes.length],
+            properties: {},
+        });
+    }
+    return components;
 }

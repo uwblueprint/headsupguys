dae409e1ba8395e43a87edc4071a93d3b85b0a5c
Aphexis <stephxca@gmail.com>
2021-06-16 21:09:21 -0400

    remove unneeded firebase related code

--- src/components/newbutton/index.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { Spacer, Flex } from "@chakra-ui/react";
-import { Header, Main, Cards, Footer } from "@components";
-import { Button, ButtonGroup } from "@chakra-ui/react";

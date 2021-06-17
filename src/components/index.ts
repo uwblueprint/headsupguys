21edc2e5ed5400ce28b34990821c2288512cdc59
Adil Kapadia <adilkapadia@Adils-MacBook-Pro.local>
2021-06-16 20:33:01 -0400

    initial commit

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 export { Header } from "./header";
 export { Logo } from "./logo";
 export { Main } from "./main";
-export { Button } from "./button";
+export { MyButton } from "./button";
 export { Cards } from "./cards";
 export { Footer } from "./footer";
 export { Layout } from "./Layout";

223c7dd8f4ea18fc50e9d4d33d4e8566c7d9aeba
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-06-20 16:06:34 -0400

    find and delete module

--- pages/api/module/delete.ts
+++ pages/api/module/delete.ts
@@ -1,10 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Module } from "../models/module";
 
 const del = async (
     req: NextApiRequest,
     res: NextApiResponse,
 ): Promise<void> => {
-    res.status(200).json({ OK: "ok" });
+    const { id } = req.query;
+    const module = await Module.findByIdAndDelete(id).exec();
+
+    if (!module)
+        return res
+            .status(404)
+            .send("The module with the given ID was not found.");
+    res.status(200).json(module);
 };
 
 export { del };

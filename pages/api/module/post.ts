3556a20b10a9330a0f70344298428152dd711853
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-06-20 15:50:50 -0400

    Update post.ts

--- pages/api/module/post.ts
+++ pages/api/module/post.ts
@@ -11,12 +11,10 @@ const post = async (
     else {
         postModule = new Module({
             title: req.body.title,
-            tool: req.body.title ? req.body.title : req.body.title.default,
-            slides: req.body.slides ? req.body.slides : req.body.slides.default,
-            status: req.body.status ? req.body.status : req.body.status.default,
-            editing: req.body.editing
-                ? req.body.editing
-                : req.body.editing.default,
+            tool: req.body.tool,
+            slides: req.body.slides,
+            status: req.body.status,
+            editing: req.body.editing,
         });
         await postModule.save();
     }

f6d17edac71f45dddb75e09bcd8f30619fa91d20
Tony Zhao <tawnyzhao@gmail.com>
2021-06-20 14:02:58 -0700

    Fix delete route to update module

--- pages/api/slide/delete.ts
+++ pages/api/slide/delete.ts
@@ -1,13 +1,23 @@
+import { Types } from "mongoose";
 import { NextApiRequest, NextApiResponse } from "next";
 import { ErrorResponse } from "types/ErrorResponse";
+import { Module } from "../models/module";
 import { Slide } from "../models/slide";
 
 const del = async (
     req: NextApiRequest,
     res: NextApiResponse<ErrorResponse>,
 ): Promise<void> => {
-    const { id } = req.query;
-    const slide = await Slide.findByIdAndDelete(id);
+    const { id, sid } = req.query;
+    const module = await Module.findById(id);
+    if (Array.isArray(sid)) {
+        res.status(400).send({ error: "Invalid slide id" });
+        return;
+    }
+    const slideId = Types.ObjectId(sid);
+    module.slides = module.slides.filter((id) => !id.equals(slideId));
+    module.save();
+    const slide = await Slide.findByIdAndDelete(sid);
 
     if (slide) {
         res.status(204).send({});

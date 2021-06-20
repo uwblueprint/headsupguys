99bb75f64c128124b1239a5b6426bd64db8ffd14
Tony Zhao <tawnyzhao@gmail.com>
2021-06-20 14:22:11 -0700

    Add type to route

--- pages/api/module/[id]/slide/index.ts
+++ pages/api/module/[id]/slide/index.ts
@@ -1,8 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { post } from "pages/api/slide/post";
+import { ErrorResponse } from "types/ErrorResponse";
 import connectDB from "../../../utils/mongoose";
 
-const index = async (req: NextApiRequest, res: NextApiResponse) => {
+const index = async (
+    req: NextApiRequest,
+    res: NextApiResponse<ErrorResponse>,
+) => {
     switch (req.method) {
         case "POST":
             post(req, res);

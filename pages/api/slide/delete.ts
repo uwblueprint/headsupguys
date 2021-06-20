449fd9394eafa7c9f55ea17a851797072146abb1
Tony Zhao <tawnyzhao@gmail.com>
2021-06-20 13:23:10 -0700

    Add slide delete

--- pages/api/slide/delete.ts
+++ pages/api/slide/delete.ts
@@ -1,10 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { ErrorResponse } from "types/ErrorResponse";
+import { Slide } from "../models/slide";
 
 const del = async (
     req: NextApiRequest,
-    res: NextApiResponse,
+    res: NextApiResponse<ErrorResponse>,
 ): Promise<void> => {
-    res.status(200).json({ OK: "ok" });
+    const { id } = req.query;
+    const slide = await Slide.findByIdAndDelete(id);
+
+    if (slide) {
+        res.status(204).send({});
+    } else {
+        res.status(404).send({ error: "Slide not found" });
+    }
+    return;
 };
 
 export { del };

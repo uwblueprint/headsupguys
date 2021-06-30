fc01628af335ca320c72296caed0587725547baf
Mayank Kanoria <mayankkanoria@gmail.com>
2021-06-30 19:39:41 -0400

    Linting fix

--- src/components/radio/index.tsx
+++ src/components/radio/index.tsx
@@ -3,39 +3,39 @@ import { useRadio, Box } from "@chakra-ui/react";
 
 interface IProps {
     value?: string | number;
-    children?: React.ReactNode
+    children?: React.ReactNode;
 }
 
 export const StyledRadio: React.FC<IProps> = (props: IProps) => {
-    const { getInputProps, getCheckboxProps } = useRadio(props)
+    const { getInputProps, getCheckboxProps } = useRadio(props);
 
-    const input = getInputProps()
-    const checkbox = getCheckboxProps()
+    const input = getInputProps();
+    const checkbox = getCheckboxProps();
 
     return (
         <Box as="label" borderColor="black" w="100%">
-        <input {...input} />
-        <Box
-            {...checkbox}
-            cursor="pointer"
-            borderWidth="1px"
-            borderRadius="md"
-            boxShadow="md"
-            borderColor="black"
-            _focus={{
-            boxShadow: "outline",
-            }}
-            _checked={{
-                bg: "teal.600",
-                color: "white",
-                borderColor: "teal.600",
-            }}
-            px={5}
-            py={3}
-            my={1}
-        >
-            {props.children}
+            <input {...input} />
+            <Box
+                {...checkbox}
+                cursor="pointer"
+                borderWidth="1px"
+                borderRadius="md"
+                boxShadow="md"
+                borderColor="black"
+                _focus={{
+                    boxShadow: "outline",
+                }}
+                _checked={{
+                    bg: "teal.600",
+                    color: "white",
+                    borderColor: "teal.600",
+                }}
+                px={5}
+                py={3}
+                my={1}
+            >
+                {props.children}
+            </Box>
         </Box>
-        </Box>
-    )
+    );
 };

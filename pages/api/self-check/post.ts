ec7052d1163b0b203d38ff08d2275d6a5ee2ed15
Christine <69013315+christineNg1028@users.noreply.github.com>
2021-06-30 21:01:57 -0400

    post self check group

--- pages/api/self-check/post.ts
+++ pages/api/self-check/post.ts
@@ -1,17 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { SelfCheckGroup } from "../models/selfCheckGroup";
+import { SelfCheckQuestion } from "../models/selfCheckQuestion";
 
 const postSelfCheck = async (
     req: NextApiRequest,
     res: NextApiResponse,
 ): Promise<void> => {
-    try {
-        const selfCheck = new SelfCheckGroup({});
-        selfCheck.save();
-        res.status(200).send(selfCheck);
-    } catch (err) {
-        res.status(400).json({ error: err });
+    // Get a list of self check questions
+    const { questions } = req.body;
+    console.log(questions);
+
+    // For each, create a SelfCheckQuestion
+    const selfCheckQuestions = await SelfCheckQuestion.insertMany(
+        questions,
+    ).catch(function (error) {
+        console.log(error); // Failure
+    });
+    console.log(selfCheckQuestions);
+
+    // Get ID of each self check question
+    const questionIDs = [];
+    for (let i = 0; i < selfCheckQuestions.length; i++) {
+        questionIDs.push(selfCheckQuestions[i]._id);
     }
+    console.log(questionIDs);
+
+    // Use IDs to create SelfCheckGroup
+    const selfCheck = new SelfCheckGroup({
+        questionIDs: questionIDs,
+    });
+    selfCheck.save();
+    res.status(200).send(selfCheck);
 };
 
 export { postSelfCheck };

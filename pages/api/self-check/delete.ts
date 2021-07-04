4a4134739e34613c74c7a640b7a8d620caefe84d
Christine <69013315+christineNg1028@users.noreply.github.com>
2021-07-04 18:04:11 -0400

    delete self check group

--- pages/api/self-check/delete.ts
+++ pages/api/self-check/delete.ts
@@ -1,10 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { SelfCheckGroup } from "../models/selfCheckGroup";
 
 const delSelfCheckByID = async (
     req: NextApiRequest,
     res: NextApiResponse,
 ): Promise<void> => {
-    res.status(200).json({ OK: "ok" });
+    const { id } = req.query;
+    console.log(id);
+    let selfCheck;
+    try {
+        selfCheck = await SelfCheckGroup.findByIdAndDelete(id).exec();
+    } catch (err) {
+        console.log("error is ", err);
+    }
+
+    if (!selfCheck) {
+        return res
+            .status(404)
+            .send({ error: "The self check with the given ID was not found." });
+    }
+
+    res.status(204).send({});
 };
 
 export { delSelfCheckByID };

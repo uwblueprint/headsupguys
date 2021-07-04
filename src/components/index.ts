b6b309487037ea850c39c2383d2da4dcd5bed7d2
Aphexis <stephxca@gmail.com>
2021-07-04 17:58:10 -0400

    create text, password, checkbox input, button

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,7 @@ export { Main } from "./main";
 export { Button } from "./button";
 export { Cards } from "./cards";
 export { Footer } from "./footer";
+export { TextInput } from "./textInput";
+export { PasswordInput } from "./passwordInput";
+export { CheckboxComp } from "./checkbox";
+export { AuthButton } from "./authButton";

329f14f4cbfa61aca3774c3cc5e535acc9101885
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-07-08 02:52:00 -0400

    selcheck Buttons

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -7,9 +7,11 @@ import {
     Text,
     Input,
     Menu,
+    ButtonGroup,
     Button,
-    Icon,
+    IconButton,
     Select,
+    Textarea,
 } from "@chakra-ui/react";
 
 import { ArrowUpIcon, ArrowDownIcon } from "@chakra-ui/icons";
@@ -36,39 +38,93 @@ export const SelfCheckQuestionCards: React.FC = () => {
     return (
         <SimpleGrid columns={1} spacing={10} px={10} py={10}>
             {(selfCheckData?.plugins ?? []).map((plugin) => (
-                <Box key={plugin.number} boxShadow={"2xl"} rounded={"md"} p={6}>
-                    <Flex alignContent="center" pb={15}>
-                        <Menu>
-                            <Heading
-                                fontSize={16}
-                                fontWeight="500"
-                                alignSelf="center"
-                                mr={6}
-                            >
-                                {plugin.number}
-                            </Heading>
-                            <Input
-                                variant="flushed"
-                                placeholder="Title"
-                                width={"full"}
-                                mr={6}
+                <Box>
+                    <Button
+                        borderWidth="2px"
+                        borderRadius="lg"
+                        p={3}
+                        mb={5}
+                        bg={"white"}
+                        borderColor="#3182CE"
+                        color="#3182CE"
+                        width={"full"}
+                        key={plugin.number}
+                        fontWeight={600}
+                    >
+                        + Question
+                    </Button>
+                    <Box
+                        borderWidth="2px"
+                        borderRadius="lg"
+                        key={plugin.number}
+                        rounded={"md"}
+                        p={6}
+                        borderColor="#C0BABA"
+                    >
+                        <Flex alignContent="center" pb={15}>
+                            <Menu>
+                                <Heading
+                                    fontSize={16}
+                                    fontWeight="500"
+                                    alignSelf="center"
+                                    mr={6}
+                                >
+                                    {plugin.number}
+                                </Heading>
+                                <Input
+                                    variant="flushed"
+                                    placeholder="Title"
+                                    width={"full"}
+                                    mr={6}
+                                />
+
+                                <Select minWidth={160} width={280} mr={6}>
+                                    <option value="option1">
+                                        Multiple Choice
+                                    </option>
+                                    <option value="option1">
+                                        Multi Select
+                                    </option>
+                                    <option value="option2">
+                                        Short Answer
+                                    </option>
+                                    <option value="option3">Long Answer</option>
+                                    <option value="option4">Slider</option>
+                                </Select>
+                            </Menu>
+                            <IconButton mr={2.5} icon={<ArrowUpIcon />} />
+                            <IconButton icon={<ArrowDownIcon />} />
+                        </Flex>
+                        <Flex alignContent="center">
+                            <Textarea
+                                resizr={"horizontal"}
+                                placeholder={plugin.questionType}
+                                mr={3}
                             />
-                            <Select minWidth={150} width={280} mr={6}>
-                                <option value="option1">Multiple Choice</option>
-                                <option value="option1">Multi Select</option>
-                                <option value="option2">Short Answer</option>
-                                <option value="option3">Long Answer</option>
-                                <option value="option4">Slider</option>
-                            </Select>
-                        </Menu>
-                        <Button mr={1}>
-                            <Icon as={ArrowUpIcon} />
-                        </Button>
-                        <Button>
-                            <Icon as={ArrowDownIcon} />
-                        </Button>
-                    </Flex>
-                    <Text fontSize={14}>{plugin.description}</Text>
+                            <ButtonGroup
+                                ml={2.5}
+                                size="sm"
+                                isAttached
+                                variant="outline"
+                            >
+                                <Button>Aa</Button>
+                                <Button>123</Button>
+                            </ButtonGroup>
+                        </Flex>
+                        <Flex direction={"rowReverse"} justify={"flex-end"}>
+                            <Button
+                                mt={15}
+                                pt={1}
+                                pb={1}
+                                pr={3}
+                                pl={3}
+                                variant="ghost"
+                                colorScheme="red"
+                            >
+                                Delete
+                            </Button>
+                        </Flex>
+                    </Box>
                 </Box>
             ))}
         </SimpleGrid>

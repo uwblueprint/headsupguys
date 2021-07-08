da6845ef81d21d81fb9e3e2540edca8250ab6850
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-07-07 21:58:11 -0400

    create Modal structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,12 +5,9 @@ export { Button } from "./button";
 export { Cards } from "./cards";
 export { Footer } from "./footer";
 export { Layout } from "./Layout";
-<<<<<<< HEAD
 export { Modal } from "./modal";
 export { TextInput } from "./textInput";
 export { PasswordInput } from "./passwordInput";
 export { CheckboxComp } from "./checkbox";
 export { AuthButton } from "./authButton";
 export { ProgressDots } from "./progressDots";
-=======
->>>>>>> Add navbar component
--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -6,76 +6,47 @@ import {
     ModalHeader,
     ModalFooter,
     ModalBody,
+    Button,
     Text,
-    Spacer,
 } from "@chakra-ui/react";
-import { Button } from "..";
 
 export interface ModalProps {
-    isOpen: boolean;
-    onCancel: () => void;
-    onConfirm: () => void;
     header: string;
     bodyText?: string;
     cancelText?: string;
     confirmText?: string;
-    alignButtonsRight?: boolean;
-    confirmButtonColorScheme?: string;
-}
+    isOpen: boolean;
+    onCancel: () => void;
+    onConfirm: () => void;
+} // TODO pass in style overrides
 
 // to control modals:
 // const { isOpen, onOpen, onClose } = useDisclosure();
 
 export const Modal: React.FC<ModalProps> = (props) => {
     const {
-        isOpen,
-        onCancel,
-        onConfirm,
         header,
         bodyText,
         cancelText = "Cancel",
         confirmText = "Confirm",
-        alignButtonsRight = true,
-        confirmButtonColorScheme,
-        ...rest
+        isOpen,
+        onCancel,
+        onConfirm,
     } = props;
 
     return (
         <>
-            <ChakraModal
-                isCentered
-                onClose={onCancel}
-                isOpen={isOpen}
-                {...rest}
-            >
+            <ChakraModal onClose={onCancel} isOpen={isOpen} isCentered>
                 <ModalOverlay />
-                <ModalContent p="8">
-                    <ModalHeader fontSize="36" fontFamily="Geogrotesque Bold">
-                        {header}
-                    </ModalHeader>
+                <ModalContent>
+                    <ModalHeader>{header}</ModalHeader>
                     <ModalBody>
                         {bodyText && <Text fontSize="lg">{bodyText}</Text>}
                         {props.children}
                     </ModalBody>
                     <ModalFooter>
-                        <Button
-                            mr="8"
-                            variant="outlineBlack"
-                            onClick={onCancel}
-                        >
-                            {cancelText}
-                        </Button>
-                        {!alignButtonsRight && <Spacer />}
-                        {/* variant="solid" to apply colorScheme */}
-                        <Button
-                            variant={
-                                confirmButtonColorScheme ? "solid" : "default"
-                            }
-                            colorScheme={confirmButtonColorScheme}
-                            onClick={onConfirm}
-                        >
-                            {confirmText}
-                        </Button>
+                        <Button onClick={onCancel}>{cancelText}</Button>
+                        <Button onClick={onConfirm}>{confirmText}</Button>
                     </ModalFooter>
                 </ModalContent>
             </ChakraModal>

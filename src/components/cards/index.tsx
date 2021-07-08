5a4cf4e9247511fe5e9226f61da3215cdb76cbe5
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-07-07 23:31:16 -0400

    additional dropdowns and icons

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
     SimpleGrid,
+    Flex,
     Box,
     Heading,
     Text,
-    Button,
+    Input,
     Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
+    Button,
+    Icon,
+    Select,
 } from "@chakra-ui/react";
+
+import { ArrowDownIcon } from "@chakra-ui/icons";
 
 import data from "@public/meta.json";
 import selfCheckData from "@public/selfCheckQuestions.json";
@@ -24,27 +26,6 @@ export const Cards: React.FC = () => {
                         {plugin.name}
                     </Heading>
                     <Text fontSize={14}>{plugin.description}</Text>
-                    <Menu>
-                        {({ isOpen }) => (
-                            <>
-                                <MenuButton
-                                    isActive={isOpen}
-                                    as={Button}
-                                    rightIcon={<ChevronDownIcon />}
-                                >
-                                    {isOpen ? "Close" : "Open"}
-                                </MenuButton>
-                                <MenuList>
-                                    <MenuItem>Download</MenuItem>
-                                    <MenuItem
-                                        onClick={() => alert("Kagebunshin")}
-                                    >
-                                        Create a Copy
-                                    </MenuItem>
-                                </MenuList>
-                            </>
-                        )}
-                    </Menu>
                 </Box>
             ))}
         </SimpleGrid>
@@ -55,10 +36,29 @@ export const SelfCheckQuestionCards: React.FC = () => {
     return (
         <SimpleGrid columns={1} spacing={10} px={10} py={10}>
             {(selfCheckData?.plugins ?? []).map((plugin) => (
-                <Box key={plugin.name}>
-                    <Heading fontSize={16} fontWeight="500" py={5}>
-                        {plugin.name}
-                    </Heading>
+                <Box key={plugin.name} boxShadow={"2xl"} rounded={"md"} p={6}>
+                    <Flex alignContent="center">
+                        <Menu>
+                            <Heading
+                                fontSize={16}
+                                fontWeight="500"
+                                alignSelf="center"
+                                mr={6}
+                            >
+                                {plugin.name}
+                            </Heading>
+                            <Input placeholder="Title" width={"full"} mr={6} />
+                            <Select fitContent width={200} mr={6}>
+                                <option value="option1">Multi Select</option>
+                                <option value="option2">Short Answer</option>
+                                <option value="option3">Long Answer</option>
+                                <option value="option4">Slider</option>
+                            </Select>
+                        </Menu>
+                        <Button>
+                            <Icon as={ArrowDownIcon} />
+                        </Button>
+                    </Flex>
                     <Text fontSize={14}>{plugin.description}</Text>
                 </Box>
             ))}

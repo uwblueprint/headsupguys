477d40b33fccc5cd6339a5676220fec122c19d71
Tony Zhao <tawnyzhao@gmail.com>
2021-07-14 16:29:43 -0700

    Add navbar component

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,9 +5,12 @@ export { Button } from "./button";
 export { Cards } from "./cards";
 export { Footer } from "./footer";
 export { Layout } from "./Layout";
+<<<<<<< HEAD
 export { Modal } from "./modal";
 export { TextInput } from "./textInput";
 export { PasswordInput } from "./passwordInput";
 export { CheckboxComp } from "./checkbox";
 export { AuthButton } from "./authButton";
 export { ProgressDots } from "./progressDots";
+=======
+>>>>>>> Add navbar component

8de0bfb39fc6a84705f13688b7a49910f87e2f68
Tony Zhao <tawnyzhao@gmail.com>
2021-07-14 16:29:09 -0700

    Global theming for button and fonts

---
+++ src/definitions/chakra/theme/fonts.ts
@@ -0,0 +1,8 @@
+import { ThemeOverride } from "@chakra-ui/react";
+
+type Fonts = Pick<ThemeOverride, "fonts">;
+
+export default {
+    heading: "Geogrotesque Bold",
+    body: "Geogrotesque",
+} as Fonts;

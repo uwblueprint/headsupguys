5a458f8f7def328589937f748dfee862af8868ed
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-07-21 22:03:27 -0400

    Module API Routes (#13)
    * routes boilerplate
    
    * directory structure
    
    * base routing
    
    * base api request
    
    * tool post and gets
    
    * Tool ID update
    
    * Update post.ts
    
    * find and delete module
    
    * fix ID getting
    
    * Update all requests
    
    * update post module post request
    
    * linting
    
    * linting changes
    
    * Revert "linting changes"
    
    This reverts commit bab97b46e811cdaf4b828ba3f0760c01ea60bff7.
    
    * Revert "linting"
    
    This reverts commit 9eceb2d3023de3080a124cd6611b963a8f0c050b.
    
    * different module location and patch fix
    
    * fix post api
    
    * Delete post.ts
    
    * Update post.ts
    
    * error clarity
    
    * post and get clarity fixes
    
    * Update post.ts
    
    * id generation (for tool)
    
    * typo fix

--- pages/api/dbTest.tsx
+++ pages/api/dbTest.tsx
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import connectDB from "./utils/mongoose";
+import { Module } from "../../../database/models/module";
 
 const dbTest = async (req: NextApiRequest, res: NextApiResponse) => {
     // Use mongoose here!
-    res.status(200).json({ name: "Hello World!!!" });
 };
 
 export default connectDB(dbTest);

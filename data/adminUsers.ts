5685a32e1116da4e8e620df7a45e93ce98e806ed
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-07-25 15:02:39 -0400

    mock admin user data

---
+++ data/adminUsers.ts
@@ -0,0 +1,24 @@
+const adminUsers = [
+    {
+        name: "Admin Name",
+        email: "admin@gmail.com",
+        role: "ADMIN",
+    },
+    {
+        name: "Admin Name",
+        email: "admin@gmail.com",
+        role: "ADMIN",
+    },
+    {
+        name: "Admin Name",
+        email: "admin@gmail.com",
+        role: "SUPER ADMIN",
+    },
+    {
+        name: "Admin Name",
+        email: "admin@gmail.com",
+        role: "ADMIN",
+    },
+];
+
+export default adminUsers;

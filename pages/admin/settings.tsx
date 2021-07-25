4f8f7f81913514823b2ed1f359301859809c1614
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-07-25 16:42:31 -0400

    add initial table

--- pages/admin/settings.tsx
+++ pages/admin/settings.tsx
@@ -1,12 +1,78 @@
 import React, { useState, useEffect } from "react";
 import { Flex, Text, Button, useDisclosure, Spacer } from "@chakra-ui/react";
-
+import { useTable } from "react-table";
 import { Modal } from "@components";
 import adminUsers from "data/adminUsers";
 
 const SettingsPage: React.FC = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const [toolsArray, setToolsArray] = useState([]);
+    const data = React.useMemo(() => adminUsers, []);
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: "USER NAME",
+                accessor: "name", // accessor is the "key" in the data
+            },
+            {
+                Header: "USER EMAIL",
+                accessor: "email",
+            },
+            {
+                Header: "USER ROLE",
+                accessor: "role",
+            },
+        ],
+        [],
+    );
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+        useTable({ columns, data });
+
+    const table = (
+        <table {...getTableProps()} style={{ border: "solid 1px blue" }}>
+            <thead>
+                {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <th
+                                {...column.getHeaderProps()}
+                                style={{
+                                    borderBottom: "solid 3px red",
+                                    background: "aliceblue",
+                                    color: "black",
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                {column.render("Header")}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map((cell) => {
+                                return (
+                                    <td
+                                        {...cell.getCellProps()}
+                                        style={{
+                                            padding: "10px",
+                                            border: "solid 1px gray",
+                                            background: "papayawhip",
+                                        }}
+                                    >
+                                        {cell.render("Cell")}
+                                    </td>
+                                );
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 
     return (
         <Flex direction="column" padding="16">
@@ -16,9 +82,9 @@ const SettingsPage: React.FC = () => {
             <Flex>
                 <Text>User Management</Text>
                 <Spacer />
-
                 <Button minWidth="140">Add User</Button>
             </Flex>
+            {table}
         </Flex>
     );
 };

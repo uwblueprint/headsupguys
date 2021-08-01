79f3d1e6c526eea455a165846b0aea316220091e
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-08-01 15:00:46 -0400

    update AddModal to use setState props

--- pages/admin/modals/addModal.tsx
+++ pages/admin/modals/addModal.tsx
@@ -1,12 +1,31 @@
 import { Modal, ModalProps } from "@components/modal";
 import React from "react";
+import { TextInput } from "@components/textInput";
+import { RoleType } from "../settings";
 
-const AddModal: React.FC<ModalProps> = (props) => {
-    const modalProps = { size: "xl", ...props };
+export interface AddModalProps extends ModalProps {
+    setEmail: (value: string) => void;
+    setRole: (value: RoleType) => void;
+}
 
+const AddModal: React.FC<AddModalProps> = (props) => {
+    const { setEmail, setRole } = props;
     return (
-        <Modal header="Add Admin" {...modalProps}>
-            {props.children}
+        <Modal size="xl" header="Add Admin" {...props}>
+            <TextInput
+                name="email"
+                label="Email"
+                errorMessage="This email already has an admin account"
+                onChange={(e) => setEmail(e.target.value)}
+                isRequired={true}
+            ></TextInput>
+            <TextInput
+                name="role"
+                label="Role"
+                errorMessage="INSERT ERROR MESSAGE HERE"
+                onChange={(e) => setRole(e.target.value as RoleType)}
+                isRequired={true}
+            ></TextInput>
         </Modal>
     );
 };

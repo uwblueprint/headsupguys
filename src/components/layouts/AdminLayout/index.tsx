65c72baeb790bd704cb8b6a80504892577accc94
Tony Zhao <tawnyzhao@gmail.com>
2021-08-01 13:38:13 -0700

    Customize theme for admin

--- src/components/layouts/AdminLayout/index.tsx
+++ src/components/layouts/AdminLayout/index.tsx
@@ -1,22 +1,25 @@
 import React from "react";
-import { Box, Grid } from "@chakra-ui/react";
+import { Box, ChakraProvider, Grid } from "@chakra-ui/react";
 
 import { Navbar, NAVBAR_WIDTH } from "@components/Navbar";
+import { adminTheme } from "@definitions/chakra/theme";
 
 export const AdminLayout: React.FC = ({ children }) => {
     return (
-        <Grid
-            templateColumns={`${NAVBAR_WIDTH} 1fr`}
-            templateRows="100vh"
-            h="100vh"
-        >
-            <Box w="100%" h="100%">
-                {/** Navbar width is set manually to keep the position fixed */}
-                <Navbar />
-            </Box>
-            <Box w="100%" h="100%" p={12}>
-                {children}
-            </Box>
-        </Grid>
+        <ChakraProvider theme={adminTheme}>
+            <Grid
+                templateColumns={`${NAVBAR_WIDTH} 1fr`}
+                templateRows="100vh"
+                h="100vh"
+            >
+                <Box w="100%" h="100%">
+                    {/** Navbar width is set manually to keep the position fixed */}
+                    <Navbar />
+                </Box>
+                <Box w="100%" h="100%" p={12}>
+                    {children}
+                </Box>
+            </Grid>
+        </ChakraProvider>
     );
 };

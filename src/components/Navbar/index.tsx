72ff165ebc3569b81e8c96d1cd3475171d4c0521
Tony Zhao <tawnyzhao@gmail.com>
2021-08-01 13:33:53 -0700

    Add highlighting on active routes navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,37 @@
 import React from "react";
-import { Flex, Heading, VStack, Spacer, Link } from "@chakra-ui/react";
+import {
+    Flex,
+    Heading,
+    VStack,
+    Spacer,
+    Link,
+    Button,
+    Text,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
+import { useRouter } from "next/dist/client/router";
 const NAVBAR_WIDTH = "280px";
 
-const NavItem = ({ children, to, ...rest }) => (
-    <Link as={NextLink} href={to}>
-        <Heading as="span" size="lg" {...rest}>
-            {children}
-        </Heading>
-    </Link>
-);
+const NavItem = ({ children, to, ...rest }) => {
+    const router = useRouter();
+    const active = to == router.pathname;
+    return (
+        <Link as={NextLink} href={to}>
+            <Button
+                variant="link"
+                color="black"
+                isActive={active}
+                _hover={{ color: "gray.700" }}
+                _active={{ fontWeight: 700, textDecoration: "underline" }}
+                size="sm"
+                fontSize="2xl"
+                {...rest}
+            >
+                {children}
+            </Button>
+        </Link>
+    );
+};
 
 const Navbar: React.FC = () => {
     return (
@@ -20,18 +42,19 @@ const Navbar: React.FC = () => {
             w={NAVBAR_WIDTH}
             h="100%"
             px={{ base: 6, md: 8, lg: 12 }}
-            py={{ base: 4, sm: 8, md: 12, xl: 20 }}
+            py={{ base: 4, sm: 8, md: 12, xl: 16 }}
             justify="space-between"
             overflowX="hidden"
             textTransform="capitalize"
+            alignItems="flex-start"
             role="navigation"
             position="fixed"
             left="0"
             top="0"
         >
-            <NavItem to="/admin" mb={8}>
+            <Text fontSize="2xl" mb={12} fontWeight={700}>
                 HeadsUpGuys
-            </NavItem>
+            </Text>
             <VStack spacing={2} align="flex-start">
                 <NavItem to="/admin/dashboard/tools">Tools</NavItem>
                 <NavItem to="/admin/dashboard/modules">Modules</NavItem>
--- src/definitions/chakra/theme/styles.ts
+++ src/definitions/chakra/theme/styles.ts
@@ -3,15 +3,5 @@ import { ThemeOverride } from "@chakra-ui/react";
 type GlobalStyles = Pick<ThemeOverride, "styles">;
 
 export default {
-    styles: {
-        global: {
-            h1: {
-                fontWeight: 500,
-                marginBottom: "0.5em",
-            },
-            p: {
-                marginBottom: "1em",
-            },
-        },
-    },
+    styles: {},
 } as GlobalStyles;

2d1befe88d74c1bd19ae1dc5d3c1d39b511c022c
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-08-04 22:42:49 -0400

    create User model

---
+++ database/models/user.ts
@@ -0,0 +1,48 @@
+import { models, model, Schema } from "mongoose";
+
+enum Role {
+    USER = "USER",
+    ADMIN = "ADMIN",
+    SUPER_ADMIN = "SUPER ADMIN",
+}
+
+export interface UserInterface {
+    _id: Schema.Types.ObjectId;
+    email: string;
+    name: string;
+    role: Role;
+    waiverSigned: boolean;
+    demographicInfo: unknown;
+}
+
+const UserSchema = new Schema<UserInterface>(
+    {
+        email: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: Role,
+            default: Role.USER,
+        },
+        waiverSigned: {
+            type: Boolean,
+            default: false,
+        },
+        demographicInfo: {
+            type: Schema.Types.Mixed,
+            required: null,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const User = models.User || model<UserInterface>("User", UserSchema);
+
+export { User, UserSchema };

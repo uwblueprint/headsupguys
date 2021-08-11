af8201b83def3897096ed662c744c892f6f53cc2
Daniel Yu <daniel.sm.yu@gmail.com>
2021-08-11 12:32:34 -0400

    Settings (#37)
    * use ...rest in Modals
    
    * mock admin user data
    
    * add initial components
    
    * add initial table
    
    * initial table styling
    
    * add edit and delete columns
    
    * pass row to edit/delete to access row.id
    
    * add modal
    
    * update mock admin data
    
    * handle 3 modal types
    
    * style modals
    
    * clean up
    
    * remove <><> around modal component
    
    * use textInput and chakra icon
    
    * create and use AddModal
    
    * create and use editModal
    
    * use ModalProps interface
    
    * use deleteModal
    
    * add header text for deleteModal
    
    * prop clean up
    
    * resolve error with passing size prop
    
    * create DeleteModal with DeleteModalProps interface
    
    * clean up DeleteModal
    
    * add RoleType, and email, role states
    
    * update AddModal to use setState props
    
    * create DropDown component
    
    * use DropDown in AddModal
    
    * provide props to EditModal
    
    * remove error message from AddModal
    
    * finish EditModal
    
    * add error logic for empty emails
    
    * add comments
    
    * clean up
    
    * create SettingsTable component
    
    * check that email is valid

---
+++ pages/admin/modals/addModal.tsx
@@ -0,0 +1,43 @@
+import { Modal, ModalProps } from "@components/modal";
+import React from "react";
+import { TextInput } from "@components/textInput";
+import { RoleType } from "../settings";
+import { Box, Flex } from "@chakra-ui/react";
+import { DropDown } from "@components/dropDown";
+
+export interface AddModalProps extends ModalProps {
+    setEmail: (value: string) => void;
+    setRole: (value: RoleType) => void;
+    emailError: string;
+}
+
+const AddModal: React.FC<AddModalProps> = (props) => {
+    const { setEmail, setRole, emailError } = props;
+
+    return (
+        <Modal size="xl" header="Add Admin" {...props}>
+            <Flex>
+                <TextInput
+                    name="email"
+                    label="Email"
+                    isInvalid={emailError != ""}
+                    errorMessage={emailError}
+                    onChange={(e) => setEmail(e.target.value)}
+                    isRequired={true}
+                ></TextInput>
+                <Box width={8} />
+                <DropDown
+                    name="role"
+                    label="Role"
+                    onChange={(e) => setRole(e.target.value as RoleType)}
+                    isRequired={true}
+                >
+                    <option value={RoleType.ADMIN}>Admin</option>
+                    <option value={RoleType.SUPER_ADMIN}>Super Admin</option>
+                </DropDown>
+            </Flex>
+        </Modal>
+    );
+};
+
+export default AddModal;

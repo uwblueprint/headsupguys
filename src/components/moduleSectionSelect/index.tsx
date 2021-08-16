9327d0ab16a20d021145ec0fe042bb6e6e64ef38
Christine <69013315+christineNg1028@users.noreply.github.com>
2021-08-16 14:21:30 -0400

    state change and conditional rendering

--- src/components/moduleSectionSelect/index.tsx
+++ src/components/moduleSectionSelect/index.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Heading, Select } from "@chakra-ui/react";
-import { Button } from "..";
 
 export interface ModuleSectionSelectProps {
     sectionNumber: number;
@@ -9,21 +8,40 @@ export interface ModuleSectionSelectProps {
 export const ModuleSectionSelect: React.FC<ModuleSectionSelectProps> = (
     props,
 ) => {
-    const { sectionNumber, ...rest } = props;
+    const { sectionNumber } = props;
+    const [contentType, setContentType] = useState("");
 
-    const styles = {};
+    const handleSelect = (e) => {
+        setContentType(e.currentTarget.value);
+    };
 
     return (
         <Box>
             <Heading size="lg">Section {sectionNumber}</Heading>
             <br />
             <Heading size="md">Select Content Type</Heading>
-            <Select w="368px" my={3} placeholder="Select Option">
+            <Select
+                w="368px"
+                my={3}
+                placeholder="Select Option"
+                onChange={handleSelect}
+            >
                 <option value="staticContent">Text / Image / Video</option>
                 <option value="multipleChoice">Multiple Choice</option>
                 <option value="multiSelect">Multiselect</option>
                 <option value="shortAnswer">Short Answer</option>
             </Select>
+            {contentType == "staticContent" ? (
+                "<StaticContent />"
+            ) : contentType == "multipleChoice" ? (
+                "<MultipleChoice />"
+            ) : contentType == "multiSelect" ? (
+                "<MultiSelect />"
+            ) : contentType == "shortAnswer" ? (
+                "<ShortAnswer />"
+            ) : (
+                <></>
+            )}
         </Box>
     );
 };

8b7882d0b72351679d732065f088c1f05ffc8379
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-08-18 18:37:59 -0500

    Update selfCheckQuestions.tsx

--- pages/selfCheckQuestions.tsx
+++ pages/selfCheckQuestions.tsx
@@ -64,7 +64,7 @@ const Home: React.FC = () => {
     };
     const changeOptionInput = (id, index, target, optionOrValue) => {
         const newList = questionList.slice(0);
-        const changeIndex = optionOrValue == "option" ? 0 : 1;0
+        const changeIndex = optionOrValue == "option" ? 0 : 1;
         newList[newList.findIndex((e) => e._id === id)].options[index][
             changeIndex
         ] = target;

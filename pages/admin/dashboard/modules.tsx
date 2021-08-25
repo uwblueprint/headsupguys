5d8b2e764e035f22a4fe71b346fd69edf80f0ae9
Tony Zhao <tawnyzhao@gmail.com>
2021-08-25 15:01:24 -0700

    Move module data to /data

--- pages/admin/dashboard/modules.tsx
+++ pages/admin/dashboard/modules.tsx
@@ -11,44 +11,10 @@ import {
 import { ModuleCard } from "@components/ModuleCard";
 import { Page } from "types/Page";
 import { AdminLayout } from "@components";
+import moduleData from "data/modules";
 
 const Modules: Page = () => {
-    const modules = React.useMemo(
-        () => [
-            {
-                moduleId: 1,
-                title: "Module Alpha",
-                tool: "Starter Tool",
-                lastUpdated: new Date("December 17, 1995 03:24:00"),
-                author: "Mayank",
-            },
-            {
-                moduleId: 2,
-                title: "Module Beta",
-                lastUpdated: new Date(Date.now()),
-                author: "Tony",
-            },
-            {
-                moduleId: 3,
-                title: "Module Gamma",
-                lastUpdated: new Date(Date.now()),
-                author: "Daniel",
-            },
-            {
-                moduleId: 4,
-                title: "Module Phi",
-                lastUpdated: new Date(Date.now()),
-                author: "Chamod",
-            },
-            {
-                moduleId: 5,
-                title: "Module Iota",
-                lastUpdated: new Date(Date.now()),
-                author: "Jenna",
-            },
-        ],
-        [],
-    );
+    const modules = React.useMemo(() => moduleData, []);
     return (
         <Stack spacing={8}>
             <Flex direction="row">

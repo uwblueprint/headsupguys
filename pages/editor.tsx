bf17ee62f290f0335b8508a4b7e3d5615ba68a98
Chamod Gamage <chamodgamage26@gmail.com>
2021-08-29 14:50:04 -0400

    sample page for component

---
+++ pages/editor.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Spacer, Flex } from "@chakra-ui/react";
+
+import {
+    Header,
+    Main,
+    Footer,
+    MarkdownEditor,
+    MarkdownRenderer,
+} from "@components";
+
+const Home: React.FC = () => {
+    const [editorText, setEditorText] = useState("Hello world!"); // stores inputted markdown
+    return (
+        <Flex direction="column" minH="100vh">
+            <Header />
+            <Main title="Text editor testing page" />
+            <div className="container">
+                <MarkdownEditor value={editorText} setValue={setEditorText} />
+            </div>
+            <MarkdownRenderer>{editorText}</MarkdownRenderer>
+            <Spacer />
+            <Footer />
+        </Flex>
+    );
+};
+
+export default Home;

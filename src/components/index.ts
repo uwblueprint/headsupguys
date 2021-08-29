5587832abdeb4d2c4d8bff68e7904e2edf6d2234
Mayank Kanoria <mayankkanoria@gmail.com>
2021-08-29 03:13:21 -0400

    Remove unused components, refactor homepage

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,3 @@
-export { Header } from "./header";
-export { Logo } from "./logo";
-export { Main } from "./main";
-export { Button } from "./button";
-export { Cards } from "./cards";
-export { Footer } from "./footer";
 export { AdminLayout } from "./layouts/AdminLayout";
 export { BuilderLayout } from "./layouts/BuilderLayout";
 export { PublicLayout } from "./layouts/PublicLayout";

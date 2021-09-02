22e7bd5ff06e2752e812cfb6ff829f581615692f
Chamod Gamage <chamodgamage26@gmail.com>
2021-09-02 18:21:55 -0400

    implement better indentation with backticks

--- src/components/markdownRenderer/index.tsx
+++ src/components/markdownRenderer/index.tsx
@@ -8,81 +8,76 @@ interface MarkdownRendererProps {
     children: string;
 }
 
-export const MarkdownRenderer: React.FC<MarkdownRendererProps> = (props) => {
-    const replacePrependedDollars = (text: string) => {
-        const splitText = text.split("$");
-        let newText = (splitText[0] + "\n" + splitText.slice(1).join(""))
-            .replaceAll(/\$/g, "")
-            .replace("\n", "");
-        return (
-            <div style={{ display: "flex", flexDirection: "row" }}>
-                {splitText.map((space, i) => (
-                    <>{i > 0 && <pre>{"  "}</pre>}</>
-                ))}
-                <MarkdownRenderer>{newText}</MarkdownRenderer>
-            </div>
-        );
-    };
+const removePrependedDollars = (text: string) => {
+    let newString = "";
+    let found = false;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === "#" || found) {
+            newString += text[i];
+        } else if (text[i] !== "$" || found) {
+            newString += text[i];
+            found = true;
+        } else {
+            newString += "` `";
+        }
+    }
+    return newString;
+};
 
+export const MarkdownRenderer: React.FC<MarkdownRendererProps> = (props) => {
     return (
         <>
-            <ReactMarkdown
-                components={{
-                    strong: ({ node, className, children, ...rest }) => (
-                        <strong>{children}</strong>
-                    ),
-                    a: ({ node, className, children, ...rest }) => (
-                        <>
-                            {node.position?.start.offset > 0 &&
-                            props.children.split("\n")[
-                                node.position?.start.line - 1
-                            ][node.position?.start.column - 2] === "$" ? (
-                                <>
-                                    <div>
-                                        <ReactPlayer controls url={rest.href} />
-                                    </div>
-                                </>
-                            ) : (
-                                <a href={rest.href}>{children}</a>
-                            )}
-                        </>
-                    ),
-                    p: ({ node, className, children, ...rest }) => {
-                        return (
-                            <div>
-                                {children.map(
-                                    (child) =>
-                                        typeof child === "string" ? (
-                                            child[child.length - 1] === "$" ? (
-                                                child.replace("$", "") //removes $ before video link
-                                            ) : child[0] === "$" ||
-                                              child.includes("\n$") ? ( //checking for initial indentation
-                                                child
-                                                    .split("\n")
-                                                    .map((el) =>
-                                                        el.includes("$")
-                                                            ? replacePrependedDollars(
-                                                                  el,
-                                                              )
-                                                            : el,
-                                                    )
-                                            ) : (
-                                                <ReactMarkdown>
-                                                    {child}
-                                                </ReactMarkdown>
-                                            ) //no indentation needed for string
-                                        ) : (
-                                            child
-                                        ), //not a string
-                                )}
-                            </div>
-                        );
-                    },
-                }}
-                className={style.markdown}
-            >
-                {props.children}
-            </ReactMarkdown>
+            {props.children.split("\n").map((line) => (
+                <>
+                    {line === "" ? (
+                        <div style={{ height: 15 }}></div>
+                    ) : line[0] === "$" ? (
+                        <BaseRenderer
+                            parentString={props.children}
+                            content={removePrependedDollars(line)}
+                        />
+                    ) : (
+                        <BaseRenderer
+                            parentString={props.children}
+                            content={line}
+                        />
+                    )}
+                </>
+            ))}
         </>
     );
 };
+
+const BaseRenderer: React.FC<{ parentString: string; content: string }> = ({
+    parentString,
+    content,
+}) => (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+        <ReactMarkdown
+            components={{
+                a: ({ node, className, children, ...rest }) => (
+                    <>
+                        {node.position?.start.offset > 0 &&
+                        parentString.split("\n")[node.position?.start.line - 1][
+                            node.position?.start.column - 2
+                        ] === "$" ? (
+                            <>
+                                <div>
+                                    <ReactPlayer controls url={rest.href} />
+                                </div>
+                            </>
+                        ) : (
+                            <a href={rest.href}>{children}</a>
+                        )}
+                    </>
+                ),
+                pre: ({ node, className, children, ...rest }) => (
+                    <pre>{"    "}</pre>
+                ),
+            }}
+            className={style.markdown}
+        >
+            {content}
+        </ReactMarkdown>
+    </div>
+);

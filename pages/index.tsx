28274a44939c2e6e1758b2495bb4221889e9041a
Christine <69013315+christineNg1028@users.noreply.github.com>
2021-09-21 20:21:28 -0400

    small ui fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,16 @@ const Home: React.FC = () => {
                             gap={10}
                         >
                             <GridItem colSpan={2}>
-                                hjkfjfgjvgjkfjfjfhjfuufjhfkjfvyjhkyjmfhkhugjhmvbgkuyfjhklguykfj
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut
+                                enim ad minim veniam, quis nostrud exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum
+                                dolore eu fugiat nulla pariatur. Excepteur sint
+                                occaecat cupidatat non proident, sunt in culpa
+                                qui officia deserunt mollit anim id est laborum.
                             </GridItem>
                             <GridItem colSpan={4}>
                                 <AspectRatio

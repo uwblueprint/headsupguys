2b6ee1386059db31a614206a2c475c43dd697bcc
xsharonhe <xsharonhe@gmail.com>
2021-09-25 13:11:09 -0400

    Add back aws functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { defaultTheme } from "@definitions/chakra/theme";
 import { Page } from "types/Page";
 import { PublicLayout } from "@components";
-//import Amplify from "aws-amplify";
+import Amplify from "aws-amplify";
 import awsExports from "../src/aws-exports";
 import "@styles/global.css";
 
-//Amplify.configure({ ...awsExports, ssr: true });
+Amplify.configure({ ...awsExports, ssr: true });
 
 type AppPropsWithLayout = AppProps & {
     Component: Page;

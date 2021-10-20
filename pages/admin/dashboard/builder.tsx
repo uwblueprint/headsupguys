4cf0471e98c82dcf5fc24fb31eb134ce461baa83
Nirmal Hegde <nirmalhegde821@gmail.com>
2021-10-19 21:41:17 -0400

    fixed functionality for new markdown on module builder

--- pages/admin/dashboard/builder.tsx
+++ pages/admin/dashboard/builder.tsx
@@ -7,6 +7,7 @@ import {
     Spacer,
     Stack,
     Box,
+    Divider,
     useDisclosure,
     VStack,
     Heading,
@@ -26,12 +27,12 @@ import Link from "next/link";
 import { Page } from "types/Page";
 import {
     BuilderLayout,
-    MarkdownEditor,
     MarkdownRenderer,
     ModulePreview,
     CheckboxComp,
     ModuleSectionSelect
 } from "@components";
+import _ from "lodash";
 
 const Builder: Page = () => {
     const {
@@ -40,6 +41,31 @@ const Builder: Page = () => {
         onOpen: sidebarOpen,
         onClose: sidebarClose,
     } = useDisclosure();
+
+    // remove once real slide state is created
+    const [mockSlide, setMockSlide] = useState({
+        checkpoint: true,
+        progressBarEnabled: true,
+        buttons: {
+            save: true,
+            print: true,
+            previous: true,
+            next: true,
+        },
+        sections: [
+            {
+                type: "staticContent",
+                padding: {
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0,
+                },
+                markdown: "Hello!",
+                alignment: "",
+            },
+        ],
+    });
     const [editorText, setEditorText] = useState("Hello world!");
     const [slideNumber, setSlide] = useState(1);
     const [maxSlides, addSlide] = useState(1);
@@ -63,6 +89,22 @@ const Builder: Page = () => {
             setSlide(prevSlide);
         }
     };
+
+    const handleNewSlide = () => {
+        mockSlide.sections.push({
+            type: "",
+            padding: {
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+            },
+            markdown: "",
+            alignment: "",
+        });
+        console.log(mockSlide);
+        setMockSlide(_.cloneDeep(mockSlide));
+    }
 
     return (
         <Stack spacing={0}>
@@ -152,34 +194,48 @@ const Builder: Page = () => {
                     </Flex>
                     {isSidebarOpen && (
                         <Box>
-                            <Container maxW="70%" py={4}>
-                                <Stack spacing={2}>
-                                    <ModuleSectionSelect editorText={editorText} setEditorText={setEditorText} sectionNumber={slideNumber} />
-                                    <Button outlineColor="black" variant="ghost">+ New Section</Button>
-                                </Stack>
-                            </Container>
-                            <Container>
-                                <Stack spacing={10} direction="row">
-                                    {
-                                        buttonOptions.map( (button) => (
-                                            <CheckboxComp
-                                                text={button}
-                                                isChecked={buttons.has(button) ? true : false}
-                                                onChange={() => {
-                                                    if(buttons.has(button)){
-                                                        const newButtons = new Set(buttons);
-                                                        newButtons.delete(button);
-                                                        setButtons(newButtons);
+                            <div style={{ overflowY: "auto", height: "75vh" }}>
+                                <Container maxW="70%" py={4}>
+                                        {/* TODO: Change mockSlide to use actual slide state */}
+                                        <Stack spacing={2}>
+                                            {mockSlide.sections.map((x, idx) => (
+                                                <div key={idx}>
+                                                    {idx > 0 && 
+                                                        <div style={{ marginTop: "5%", marginBottom: "5%" }}>
+                                                            <Divider />
+                                                        </div>
                                                     }
-                                                    else{
-                                                        setButtons(new Set(buttons).add(button));
-                                                    }
-                                                }}
-                                            />
-                                        ))
-                                    }
-                                </Stack>
-                            </Container>
+                                                    <ModuleSectionSelect slide={mockSlide} slideNumber={idx} setSlide={setMockSlide} />
+                                                </div>
+                                            ))}
+                                            <br />
+                                            <Button onClick={handleNewSlide} colorScheme="black" variant="outline">+ New Section</Button>
+                                        </Stack>
+                                </Container>
+                                <Container>
+                                    <Stack spacing={10} direction="row">
+                                        {
+                                            buttonOptions.map( (button) => (
+                                                <CheckboxComp
+                                                    text={button}
+                                                    isChecked={buttons.has(button) ? true : false}
+                                                    onChange={() => {
+                                                        if(buttons.has(button)){
+                                                            const newButtons = new Set(buttons);
+                                                            newButtons.delete(button);
+                                                            setButtons(newButtons);
+                                                        }
+                                                        else{
+                                                            setButtons(new Set(buttons).add(button));
+                                                        }
+                                                    }}
+                                                />
+                                            ))
+                                        }
+                                    </Stack>
+                                    <br />
+                                </Container>
+                            </div>
                         </Box>
                     )}
                 </Box>
--- src/components/moduleSectionSelect/index.tsx
+++ src/components/moduleSectionSelect/index.tsx
@@ -1,36 +1,75 @@
-import React, { useState } from "react";
+import React from "react";
 import {
     Box,
     HStack,
-    Grid,
-    GridItem,
     Stack,
     Heading,
     Text,
     Select,
     Input,
 } from "@chakra-ui/react";
+import _ from "lodash";
 import { MarkdownEditor } from "@components";
 
 export interface ModuleSectionSelectProps {
-    sectionNumber: number;
-    editorText: string;
-    setEditorText: React.Dispatch<React.SetStateAction<string>>;
+    slide: {
+        checkpoint: boolean;
+        progressBarEnabled: boolean;
+        buttons: {
+            save: boolean;
+            print: boolean;
+            previous: boolean;
+            next: boolean;
+        };
+        sections: {
+            type: string; //markdown, mc, ms, sa
+            padding: {
+                top: number;
+                right: number;
+                bottom: number;
+                left: number;
+            };
+            markdown?: string; //stores markdown content, only applies to md component
+            alignment?: string; //on frontend this will be a dropdown
+        }[];
+    };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    setSlide: React.SetStateAction<any>; // TYPE IS SLIDE RIGHT ABOVE
+    slideNumber: number;
 }
 
 export const ModuleSectionSelect: React.FC<ModuleSectionSelectProps> = (
     props,
 ) => {
-    const { sectionNumber, editorText, setEditorText } = props;
-    const [contentType, setContentType] = useState("");
+    const { slide, setSlide, slideNumber } = props;
+
+    const paddingMap = {
+        T: "top",
+        R: "right",
+        B: "bottom",
+        L: "left",
+    };
 
     const handleSelect = (e) => {
-        setContentType(e.currentTarget.value);
+        slide.sections[slideNumber].type = e.currentTarget.value;
+        setSlide(_.cloneDeep(slide));
+    };
+
+    const handleMarkdownChange = (text) => {
+        console.log("markdownchange");
+        slide.sections[slideNumber].markdown = text;
+        console.log("slide", slide);
+        setSlide(_.cloneDeep(slide));
+    };
+
+    const handlePaddingChange = (padVal) => {
+        slide.sections[slideNumber].padding[paddingMap[padVal]];
+        setSlide(_.cloneDeep(slide));
     };
 
     return (
         <Box>
-            <Heading size="lg">Section {sectionNumber}</Heading>
+            <Heading size="lg">Section {slideNumber + 1}</Heading>
             <br />
             <Heading size="md">Select Content Type</Heading>
             <Select
@@ -44,29 +83,30 @@ export const ModuleSectionSelect: React.FC<ModuleSectionSelectProps> = (
                 <option value="multiSelect">Multiselect</option>
                 <option value="shortAnswer">Short Answer</option>
             </Select>
-            {contentType == "staticContent" ? (
+            {slide.sections[slideNumber].type == "staticContent" ? (
                 <Stack spacing={2}>
                     <MarkdownEditor
-                        value={editorText}
-                        setValue={setEditorText}
+                        value={slide.sections[slideNumber].markdown || ""}
+                        setValue={handleMarkdownChange}
                     />
-                    <Grid templateColumns="repeat(6, 1fr)" gap={4}>
-                        <GridItem colSpan={2}>
-                            <Heading size="sm">Padding (%)</Heading>
-                        </GridItem>
-                        {["T", "R", "B", "L"].map((padding) => (
-                            <HStack key={padding}>
-                                <Text>{padding}&nbsp;</Text>
-                                <Input width="50%" placeholder="0%" />
+                    <HStack spacing={10}>
+                        <Heading size="sm">Padding&nbsp;(%)</Heading>
+                        {["T", "R", "B", "L"].map((padVal) => (
+                            <HStack>
+                                <Text>{padVal}&nbsp;</Text>
+                                <Input
+                                    onChange={() => handlePaddingChange(padVal)}
+                                    placeholder="0%"
+                                />
                             </HStack>
                         ))}
-                    </Grid>
+                    </HStack>
                 </Stack>
-            ) : contentType == "multipleChoice" ? (
+            ) : slide.sections[slideNumber].type == "multipleChoice" ? (
                 "<MultipleChoice />"
-            ) : contentType == "multiSelect" ? (
+            ) : slide.sections[slideNumber].type == "multiSelect" ? (
                 "<MultiSelect />"
-            ) : contentType == "shortAnswer" ? (
+            ) : slide.sections[slideNumber].type == "shortAnswer" ? (
                 "<ShortAnswer />"
             ) : (
                 <></>

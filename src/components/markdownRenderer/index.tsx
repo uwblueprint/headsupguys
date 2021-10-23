6cde73f94718e084402449b8c90cf0e982c6c8b2
chamod-gamage <chamodgamage26@gmail.com>
2021-10-23 12:51:49 -0400

    allow for injection of string by variable name into md renderer

--- src/components/markdownRenderer/index.tsx
+++ src/components/markdownRenderer/index.tsx
@@ -6,21 +6,35 @@ import ReactPlayer from "react-player";
 
 interface MarkdownRendererProps {
     children: string;
+    variables?: any; //retype this later
 }
 
-export const MarkdownRenderer: React.FC<MarkdownRendererProps> = (props) => {
+const stringConversionObj = {
+    "$[": "[",
+    "<>[": "[",
+};
+
+export const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({
+    children,
+    variables,
+}) => {
     return (
         <>
-            {props.children ? (
-                props.children.split("\n").map((line) => (
-                    <>
-                        {line === "" ? (
-                            <div className="newline" />
-                        ) : (
-                            <BaseRenderer content={line} />
-                        )}
-                    </>
-                ))
+            {children ? (
+                children
+                    .split("\n")
+                    .map((line) => (
+                        <>
+                            {line === "" ? (
+                                <div className="newline" />
+                            ) : (
+                                <BaseRenderer
+                                    variables={variables ? variables : null}
+                                    content={line}
+                                />
+                            )}
+                        </>
+                    ))
             ) : (
                 <div className="newline" />
             )}
@@ -28,7 +42,10 @@ export const MarkdownRenderer: React.FC<MarkdownRendererProps> = (props) => {
     );
 };
 
-const BaseRenderer: React.FC<{ content: string }> = ({ content }) => (
+const BaseRenderer: React.FC<{ content: string; variables?: any }> = ({
+    content,
+    variables,
+}) => (
     <div className="md-row">
         <ReactMarkdown
             components={{
@@ -40,6 +57,16 @@ const BaseRenderer: React.FC<{ content: string }> = ({ content }) => (
                                     <ReactPlayer controls url={rest.href} />
                                 </div>
                             </>
+                        ) : content.includes("<>[") ? (
+                            <>
+                                <div>
+                                    {variables
+                                        ? variables[rest.href]
+                                            ? variables[rest.href]
+                                            : ""
+                                        : `<Data for ${rest.href}>`}
+                                </div>
+                            </>
                         ) : (
                             <a href={rest.href}>{children}</a>
                         )}
@@ -48,7 +75,17 @@ const BaseRenderer: React.FC<{ content: string }> = ({ content }) => (
             }}
             className={style.markdown + " right"}
         >
-            {content.replace("$[", "[")}
+            {content.replace(
+                new RegExp(
+                    Object.keys(stringConversionObj)
+                        .map((key) =>
+                            key.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"),
+                        )
+                        .join("|"),
+                    "gi",
+                ),
+                (str) => stringConversionObj[str],
+            )}
         </ReactMarkdown>
     </div>
 );

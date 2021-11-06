9513ef83a886de458d91656f5155b5d790b3498e
Tony Zhao <tawnyzhao@gmail.com>
2021-11-06 10:26:46 -0600

    Componentize header

---
+++ src/pages/module-builder/Header.tsx
@@ -0,0 +1,87 @@
+import {
+    Flex,
+    Button,
+    Spacer,
+    Stack,
+    Box,
+    Heading,
+    HStack,
+    Editable,
+    EditablePreview,
+    EditableInput,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { Dispatch } from "react";
+import { INITIAL_STATE, ModuleState } from "pages/admin/dashboard/builder";
+
+const Header = ({
+    dispatch,
+    state,
+    handleSaveModule,
+}: {
+    dispatch: Dispatch<{ type: string; value: unknown }>;
+    state: ModuleState;
+    handleSaveModule: () => void;
+}): React.ReactElement => {
+    const handleNullTitleErrors = (e) => {
+        const target = e.target as HTMLInputElement;
+        if (target.value === "") {
+            dispatch({ type: "changeTitle", value: INITIAL_STATE.title });
+        }
+    };
+
+    return (
+        <Stack spacing={2} p={6}>
+            <Box>
+                <Link href="modules">
+                    <Button variant="link">{"<"} Exit Builder</Button>
+                </Link>
+            </Box>
+            <Flex>
+                <Heading>
+                    <Editable
+                        defaultValue={INITIAL_STATE.title}
+                        value={state.title}
+                        onChange={(title) =>
+                            dispatch({ type: "changeTitle", value: title })
+                        }
+                        onBlur={(e) => {
+                            handleNullTitleErrors(e);
+                        }}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                handleNullTitleErrors(e);
+                            }
+                        }}
+                        width={350}
+                    >
+                        <EditablePreview />
+                        <EditableInput />
+                    </Editable>
+                </Heading>
+                <Spacer />
+                <HStack spacing={2}>
+                    <Button
+                        variant="outline"
+                        onClick={() => {
+                            // TODO: implement discard
+                            console.log("module discard clicked");
+                        }}
+                    >
+                        Discard
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            handleSaveModule();
+                        }}
+                        isDisabled={state.title === ""}
+                    >
+                        Save
+                    </Button>
+                </HStack>
+            </Flex>
+        </Stack>
+    );
+};
+
+export default Header;

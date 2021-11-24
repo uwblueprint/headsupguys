9db3d4637f104644e28b7274b14c5e8ca9928ceb
Aphexis <stephxca@gmail.com>
2021-11-23 21:19:25 -0500

    update module on tool link, update toast on tool builder

--- pages/api/tool/post.ts
+++ pages/api/tool/post.ts
@@ -1,12 +1,36 @@
 import { Tool } from "database/models/tool";
+import { Module } from "database/models/module";
 import { NextApiRequest, NextApiResponse } from "next";
 const post = async (
     req: NextApiRequest,
     res: NextApiResponse,
 ): Promise<void> => {
-    await Tool.create(req.body)
-        .then((tool) => res.status(200).json(tool))
-        .catch((err) => res.status(400).send(err));
+    try {
+        const tool = await Tool.create(req.body);
+        //If the module contains a module id...
+        if (req.body.linkedModuleID) {
+            //If said module id is present but invalid (not an object ID), return and error
+            const module = await Module.findById(req.body.linkedModuleID).catch(
+                (err) => res.status(400).send(err),
+            );
+            //If the module isn't null and isn't already linked to another tool, assign the tool's ID to the module
+            if (!module) {
+                return res.status(400).send({
+                    error: "Invalid moduleID.",
+                });
+            }
+            if (module.toolID) {
+                return res.status(400).send({
+                    error: "Module is already linked to a tool.",
+                });
+            }
+            module.toolID = tool._id;
+            await module.save();
+        }
+        res.status(200).json(tool);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 };
 
 export default post;
--- pages/api/tool/update.ts
+++ pages/api/tool/update.ts
@@ -1,4 +1,5 @@
 import { Tool } from "database/models/tool";
+import { Module } from "database/models/module";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const update = async (
@@ -6,9 +7,32 @@ const update = async (
     res: NextApiResponse,
 ): Promise<void> => {
     const { id } = req.query;
-    await Tool.findByIdAndUpdate(id, req.body, { new: true })
-        .then((tool) => res.status(200).json(tool))
-        .catch((err) => res.status(500).send(err));
+    try {
+        const tool = await Tool.findByIdAndUpdate(id, req.body, { new: true });
+        //If the module contains a module id...
+        if (req.body.linkedModuleID) {
+            //If said module id is present but invalid (not an object ID), return and error
+            const module = await Module.findById(req.body.linkedModuleID).catch(
+                (err) => res.status(400).send(err),
+            );
+            //If the module isn't null and isn't already linked to another tool, assign the tool's ID to the module
+            if (!module) {
+                return res.status(400).send({
+                    error: "Invalid moduleID.",
+                });
+            }
+            if (module.toolID) {
+                return res.status(400).send({
+                    error: "Module is already linked to a tool.",
+                });
+            }
+            module.toolID = tool._id;
+            await module.save();
+        }
+        res.status(200).json(tool);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
 export default update;

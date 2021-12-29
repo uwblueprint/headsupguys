1f3ab19fd7ec9bf27e3b037feb30f3c47462da2f
Nirmal Hegde <nirmalhegde821@gmail.com>
2021-12-28 19:38:33 -0500

    completed mobile view

--- pages/module/[module].tsx
+++ pages/module/[module].tsx
@@ -1,45 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import Error from "next/error";
 import { Page } from "types/Page";
 import axios from "axios";
-import Document from "src/pages/module-builder/Document";
+import {
+    ModulePreview,
+    MarkdownRenderer,
+    MultipleChoicePreview,
+    MultiSelectPreview,
+} from "@components";
+import useSWR from "swr";
+import { Spinner } from "@chakra-ui/react";
+
+const fetcher = async (url) => {
+    const response = await axios({
+        method: "GET",
+        url,
+        params: { includeSlide: true },
+    });
+    return response.data;
+};
 
 const Module: Page = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
     const router = useRouter();
     const { module } = router.query;
-    const moduleExists = async () => {
-        try {
-            const existingModules = await axios({
-                method: "GET",
-                url: "/api/module/getAll",
-            });
+    const { data, error } = useSWR(`/api/module/${module}`, fetcher);
 
-            for (const existingModule in existingModules) {
-                if (existingModule === module) {
-                    return false;
-                }
-            }
-
-            return true;
-        } catch (err) {
-            console.log(err);
+    function goNextSlide() {
+        if (currentSlide < data.slides.length) {
+            setCurrentSlide(currentSlide + 1);
         }
-    };
+    }
 
-    if (moduleExists()) {
+    function goPrevSlide() {
+        if (currentSlide != 0) {
+            setCurrentSlide(currentSlide - 1);
+        }
+    }
+
+    if (error) {
+        return <Error statusCode={404} />;
+    } else if (!data) {
         return (
-            <div>
-                <Document
-                    state={state}
-                    isSidebarOpen={isSidebarOpen}
-                    sidebarOpen={sidebarOpen}
-                    sidebarClose={sidebarClose}
+            <div style={{ height: "100vh" }}>
+                <Spinner
+                    top="50%"
+                    left="50%"
+                    transform="-webkit-translate(-50%, -50%)"
+                    color="brand.lime"
+                    size="xl"
                 />
             </div>
         );
     } else {
-        return <Error statusCode={404} />;
+        return (
+            <ModulePreview
+                previous={data.slides[currentSlide].buttons.previous}
+                next={data.slides[currentSlide].buttons.next}
+                save={data.slides[currentSlide].buttons.save}
+                print={data.slides[currentSlide].buttons.print}
+                progressValue={(currentSlide / data.slides.length) * 100}
+                variant="mobile"
+                goNextSlide={() => goNextSlide()}
+                goPrevSlide={() => goPrevSlide()}
+            >
+                {data.slides[currentSlide].sections.map((section) => {
+                    let sectionPreview;
+                    if (section.type === "staticContent") {
+                        sectionPreview = (
+                            <MarkdownRenderer>
+                                {section.markdown}
+                            </MarkdownRenderer>
+                        );
+                    } else if (section.type === "multipleChoice") {
+                        sectionPreview = (
+                            <MultipleChoicePreview
+                                question={section.multipleChoice.question}
+                                options={section.multipleChoice.options}
+                                variant="mobile"
+                                columns={section.properties.columns}
+                            />
+                        );
+                    } else if (section.type === "multiSelect") {
+                        sectionPreview = (
+                            <MultiSelectPreview
+                                question={section.multiSelect.question}
+                                options={section.multiSelect.options}
+                                variant="mobile"
+                                columns={section.properties.columns}
+                            />
+                        );
+                    }
+                    return sectionPreview;
+                }, "")}
+            </ModulePreview>
+        );
     }
 };
 

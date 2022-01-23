937fc6109861fbe4e57c4017fd91b9ea0f61c97f
Neel Ismail <neelismail01@gmail.com>
2022-01-23 15:55:27 -0500

    feat/ adding google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { AppProps } from "next/app";
+import Script from "next/script";
+import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import { defaultTheme } from "@definitions/chakra/theme";
 import { Page } from "types/Page";
@@ -16,12 +18,32 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
     const Layout = Component.layout || PublicLayout;
+
     return (
-        <ChakraProvider theme={defaultTheme}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </ChakraProvider>
+        <>
+            {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && (
+                <>
+                    <Script
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+                        strategy="afterInteractive"
+                    />
+                    <Script id="google-analytics" strategy="afterInteractive">
+                        {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){window.dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+                        `}
+                    </Script>
+                </>
+            )}
+            <ChakraProvider theme={defaultTheme}>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ChakraProvider>
+        </>
     );
 }
 

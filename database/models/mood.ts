cc89ad7562d772c90bd0fb12c528d3e121aca787
Jennifer Lu <jenniferlugm@gmail.com>
2022-03-27 18:52:27 -0400

    Remove date from model

--- database/models/mood.ts
+++ database/models/mood.ts
@@ -5,7 +5,6 @@ export interface MoodInterface {
     username: string;
     entries: [
         {
-            date: number; // yyyymmdd format (for uniqueness)
             timestamp: Date; // ISO format
             moodScore: number;
             description: string;
@@ -24,15 +23,10 @@ const MoodSchema = new Schema<MoodInterface>(
         entries: {
             type: [
                 {
-                    date: {
-                        type: Number,
-                        required: true,
-                        index: true,
-                        unique: true,
-                    },
                     timestamp: {
                         type: Date,
                         required: true,
+                        index: true,
                     },
                     moodScore: { type: Number, required: true, min: 0, max: 4 },
                     description: { type: String, default: "" },
@@ -45,6 +39,23 @@ const MoodSchema = new Schema<MoodInterface>(
         timestamps: true,
     },
 );
+
+MoodSchema.pre("save", async function (next) {
+    const dates = new Set();
+    this.entries.forEach((entry) => {
+        const date = new Date(entry.timestamp);
+        const day =
+            date.getDate().toString() +
+            date.getMonth().toString() +
+            date.getFullYear().toString();
+        dates.add(day);
+    });
+    if (dates.size < this.entries.length) {
+        next(new Error("Duplicate dates in entries."));
+    } else {
+        next();
+    }
+});
 
 const Mood = models.Mood || model("Mood", MoodSchema);
 
